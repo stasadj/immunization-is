d0b42454e59fec3c92697b3b8f204986eb3896a1
albertmakan <makanalbert@gmail.com>
2022-01-06 20:46:39 +0100

    moved generated classes to src; refactored marshaller & unmarshaller services

--- backend/src/main/java/com/immunization/backend/DatabaseRetrievalExample.java
+++ backend/src/main/java/com/immunization/backend/DatabaseRetrievalExample.java
@@ -15,87 +15,87 @@ import com.immunization.backend.util.AuthenticationUtilities.ConnectionPropertie
 
 
 public class DatabaseRetrievalExample {
-    
-	public static void main(String[] args) throws Exception {
-		DatabaseRetrievalExample.run(AuthenticationUtilities.loadProperties(), args);
-	}
-    
+
+    public static void main(String[] args) throws Exception {
+        DatabaseRetrievalExample.run(AuthenticationUtilities.loadProperties(), args);
+    }
+
     /**
      * conn XML DB connection properties
      * args[0] Should be the collection ID to access
      * args[1] Should be the document ID to store in the collection
      */
-    public static void run(ConnectionProperties conn, String args[]) throws Exception {
-       
+    public static void run(ConnectionProperties conn, String[] args) throws Exception {
 
-    	System.out.println("[INFO] " + DatabaseRetrievalExample.class.getSimpleName());
-    	
-    	// initialize collection and document identifiers
+
+        System.out.println("[INFO] " + DatabaseRetrievalExample.class.getSimpleName());
+
+        // initialize collection and document identifiers
         String collectionId = null;
-		String documentId = null; 
-        
+        String documentId = null;
+
         if (args.length == 2) {
-        	
-        	System.out.println("[INFO] Passing the arguments... ");
-        	
-        	collectionId = args[0];
-        	documentId = args[1];
+
+            System.out.println("[INFO] Passing the arguments... ");
+
+            collectionId = args[0];
+            documentId = args[1];
         } else {
-        	
-        	System.out.println("[INFO] Using defaults.");
-        	
-        	collectionId = "/db/Team404/digitalni_sertifikat";
-        	documentId = "ds.xml";
+
+            System.out.println("[INFO] Using defaults.");
+
+            collectionId = "/db/Team404/digitalni_sertifikat";
+            documentId = "ds.xml";
         }
 
         System.out.println("\t- collection ID: " + collectionId);
-    	System.out.println("\t- document ID: " + documentId + "\n");
-        
+        System.out.println("\t- document ID: " + documentId + "\n");
+
         // initialize database driver
-    	System.out.println("[INFO] Loading driver class: " + conn.driver);
+        System.out.println("[INFO] Loading driver class: " + conn.driver);
         Class<?> cl = Class.forName(conn.driver);
-        
+
         Database database = (Database) cl.newInstance();
         database.setProperty("create-database", "true");
-        
+
         DatabaseManager.registerDatabase(database);
-        
+
         Collection col = null;
         XMLResource res = null;
-        
-        try {    
+
+        try {
             // get the collection
-        	System.out.println("[INFO] Retrieving the collection: " + collectionId);
+            System.out.println("[INFO] Retrieving the collection: " + collectionId);
             col = DatabaseManager.getCollection(conn.uri + collectionId);
             col.setProperty(OutputKeys.INDENT, "yes");
-            
+
             System.out.println("[INFO] Retrieving the document: " + documentId);
-            res = (XMLResource)col.getResource(documentId);
-            
-            if(res == null) {
+            res = (XMLResource) col.getResource(documentId);
+
+            if (res == null) {
                 System.out.println("[WARNING] Document '" + documentId + "' can not be found!");
             } else {
-            	
-            	System.out.println("[INFO] Showing the document as XML resource: ");
-            	System.out.println(res.getContent());
-                
+
+                System.out.println("[INFO] Showing the document as XML resource: ");
+                System.out.println(res.getContent());
+
             }
         } finally {
             //don't forget to clean up!
-            
-            if(res != null) {
-                try { 
-                	((EXistResource)res).freeResources(); 
+
+            if (res != null) {
+                try {
+                    ((EXistResource) res).freeResources();
                 } catch (XMLDBException xe) {
-                	xe.printStackTrace();
+                    xe.printStackTrace();
                 }
             }
-            
-            if(col != null) {
-                try { 
-                	col.close(); 
+
+            if (col != null) {
+                try {
+                    col.close();
                 } catch (XMLDBException xe) {
-                	xe.printStackTrace();
+                    xe.printStackTrace();
                 }
             }
         }
---
+++ backend/src/test/java/com/immunization/backend/JaxbParsingTest.java
@@ -0,0 +1,55 @@
+package com.immunization.backend;
+
+import com.immunization.backend.model.digitalni_sertifikat.DigitalniSertifikat;
+import com.immunization.backend.model.izvestaj_o_imunizaciji.IzvestajOImunizaciji;
+import com.immunization.backend.model.potvrda_o_vakcinaciji.PotvrdaOVakcinaciji;
+import com.immunization.backend.model.saglasnost.ObrazacSaglasnostiZaImunizaciju;
+import com.immunization.backend.model.zahtev_za_sertifikat.ZahtevZaSertifikat;
+import com.immunization.backend.service.UnmarshallerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.immunization.backend.model.interesovanje.IskazivanjeInteresovanjaZaVakcinaciju;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@SpringBootTest
+public class JaxbParsingTest {
+
+    @Autowired
+    private UnmarshallerService unmarshallerService;
+
+    @Test
+    public void testUnmarshalling() throws IOException {
+        System.out.println("TEST digitalni_sertifikat");
+        String xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/digitalni_sertifikat.xml")));
+        DigitalniSertifikat sertifikat = (DigitalniSertifikat) unmarshallerService.unmarshal(xmlString);
+        System.out.println("Digitalni sertifikat date: " + sertifikat.getDatumIzdavanja());
+        System.out.println("Digitalni sertifikat osoba: " + sertifikat.getLicniPodaci().getPol());
+        System.out.println("Digitalni sertifikat broj doza osobe: " + sertifikat.getVakcinacija().getVakcina().size());
+        sertifikat.getVakcinacija().getVakcina().forEach(vakc -> System.out.println(vakc.getProizvodjacSerija()));
+
+        System.out.println("TEST interesovanje");
+        xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/interesovanje.xml")));
+        IskazivanjeInteresovanjaZaVakcinaciju interesovanje = (IskazivanjeInteresovanjaZaVakcinaciju) unmarshallerService.unmarshal(xmlString);
+        System.out.println("opstina: " + interesovanje.getZeljenaOpstinaVakcinacije());
+
+        System.out.println("TEST izvestaj_o_imunizaciji");
+        xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/izvestaj_o_imunizaciji.xml")));
+        IzvestajOImunizaciji izvestaj = (IzvestajOImunizaciji) unmarshallerService.unmarshal(xmlString);
+
+        System.out.println("TEST potvrda_o_vakcinaciji");
+        xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/potvrda_o_vakcinaciji.xml")));
+        PotvrdaOVakcinaciji potvrda = (PotvrdaOVakcinaciji) unmarshallerService.unmarshal(xmlString);
+
+        System.out.println("TEST saglasnost");
+        xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/saglasnost.xml")));
+        ObrazacSaglasnostiZaImunizaciju saglasnost = (ObrazacSaglasnostiZaImunizaciju) unmarshallerService.unmarshal(xmlString);
+
+        System.out.println("TEST zahtev_za_sertifikat");
+        xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/zahtev_za_sertifikat.xml")));
+        ZahtevZaSertifikat zahtev = (ZahtevZaSertifikat) unmarshallerService.unmarshal(xmlString);
+    }
+}
