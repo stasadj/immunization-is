922984029c857eac809c374a8e98a71f93a425c8
Dina Petrov <dina.petrov@gmail.com>
2022-01-13 09:21:40 +0100

    added missing test console prints

--- backend/src/test/java/com/immunization/backend/JaxbParsingTest.java
+++ backend/src/test/java/com/immunization/backend/JaxbParsingTest.java
@@ -28,7 +28,7 @@ public class JaxbParsingTest {
 
     @Test
     public void testUnmarshalling() throws IOException, MarshalException {
-        System.out.println("TEST digitalni_sertifikat ");
+        System.out.println(" ======================== TEST digitalni_sertifikat ======================== ");
         String xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/digitalni_sertifikat.xml")));
         DigitalniSertifikat sertifikat = (DigitalniSertifikat) unmarshallerService.unmarshal(xmlString);
         System.out.println("Digitalni sertifikat date: " + sertifikat.getDatumIzdavanja());
@@ -36,29 +36,34 @@ public class JaxbParsingTest {
         System.out.println("Digitalni sertifikat broj doza osobe: " + sertifikat.getVakcinacija().getVakcina().size());
         sertifikat.getVakcinacija().getVakcina().forEach(vakc -> System.out.println("Proizvodjac vakcine: " + vakc.getProizvodjacSerija().trim()));
 
-        System.out.println("TEST interesovanje");
+        System.out.println(" ======================== TEST interesovanje ======================== ");
         xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/interesovanje.xml")));
         IskazivanjeInteresovanjaZaVakcinaciju interesovanje = (IskazivanjeInteresovanjaZaVakcinaciju) unmarshallerService.unmarshal(xmlString);
         System.out.println("opstina: " + interesovanje.getZeljenaOpstinaVakcinacije());
 
-        System.out.println("TEST izvestaj_o_imunizaciji");
+        System.out.println(" ======================== TEST izvestaj_o_imunizaciji ======================== ");
         xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/izvestaj_o_imunizaciji.xml")));
         IzvestajOImunizaciji izvestaj = (IzvestajOImunizaciji) unmarshallerService.unmarshal(xmlString);
+        System.out.println("broj interesovanja: " + izvestaj.getBrojDokumenataOInteresovanju());
 
-        System.out.println("TEST potvrda_o_vakcinaciji");
+
+        System.out.println(" ======================== TEST potvrda_o_vakcinaciji ======================== ");
         xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/potvrda_o_vakcinaciji.xml")));
         PotvrdaOVakcinaciji potvrda = (PotvrdaOVakcinaciji) unmarshallerService.unmarshal(xmlString);
+        System.out.println("datum izdavanja: " + potvrda.getDatumIzdavanjaPotvrde());
 
-        System.out.println("TEST saglasnost");
+        System.out.println(" ======================== TEST saglasnost ======================== ");
         xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/saglasnost.xml")));
         ObrazacSaglasnostiZaImunizaciju saglasnost = (ObrazacSaglasnostiZaImunizaciju) unmarshallerService.unmarshal(xmlString);
+        System.out.println("radni status pacijenta: " + saglasnost.getInformacijeOPacijentu().getRadniStatus().getValue());
 
-        System.out.println("TEST zahtev_za_sertifikat");
+        System.out.println(" ======================== TEST zahtev_za_sertifikat ======================== ");
         xmlString = new String(Files.readAllBytes(Paths.get("./src/main/resources/documents/zahtev_za_sertifikat.xml")));
         ZahtevZaSertifikat zahtev = (ZahtevZaSertifikat) unmarshallerService.unmarshal(xmlString);
+        System.out.println("broj pasosa podnosioca: " + zahtev.getPodnosilacZahteva().getBrojPasosa().getValue());
 
         String result = marshallerService.marshal(zahtev);
-        System.out.println("TEST Marshaller: ");
+        System.out.println(" ======================== TEST Marshaller:  ======================== ");
         System.out.println(result);
 
     }
