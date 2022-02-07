79cb1654edbc03638a0cafe3d34ddc192416a0d5
Dina Petrov <dina.petrov@gmail.com>
2022-02-07 17:40:54 +0100

    saving documents based on JMBG, added UUIDService just in case

--- portal/src/main/java/com/immunization/portal/service/InteresovanjeService.java
+++ portal/src/main/java/com/immunization/portal/service/InteresovanjeService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.immunization.common.exception.BadRequestException;
 import com.immunization.common.model.interesovanje.IskazivanjeInteresovanjaZaVakcinaciju;
 import com.immunization.common.repository.Exist;
+import com.immunization.common.service.UUIDService;
 
 import lombok.AllArgsConstructor;
 
@@ -21,11 +22,14 @@ public class InteresovanjeService {
 		//TODO check if person in document already has an Interesovanje from before in database	
 		
 		try {
-			//TODO generate new document UUID and document name
-			String id = exist.save("PROBA.xml", interesovanje);
+			//TODO generate new document id and document name??
+			//String newId = uuidService.getUUID();
+			//String documentName = exist.save("interesovanje" + newId + ".xml", interesovanje);
+			
+			exist.save(interesovanje.getPacijent().getJmbg() + ".xml", interesovanje);
 			return interesovanje;
 			
-			//TODO extract and save metadata
+			//TODO extract and save metadata here
 			
 		} catch (Exception e) {
 			// Exist exception
