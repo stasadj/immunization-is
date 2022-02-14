29ce2f273296ad49150acf0c5384f8e278789e83
Dina Petrov <dina.petrov@gmail.com>
2022-02-14 16:19:02 +0100

    Started Interesovanje creation form

--- portal-frontend/src/app/app.module.ts
+++ portal-frontend/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { CreateInteresovanjeComponent } from './create-interesovanje/create-interesovanje.component';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CreateInteresovanjeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatCardModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckboxModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ portal-frontend/src/app/create-interesovanje/create-interesovanje.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { Interesovanje } from '../model/Interesovanje';
+import { Form, FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+    selector: 'app-create-interesovanje',
+    templateUrl: './create-interesovanje.component.html',
+    styleUrls: ['./create-interesovanje.component.less']
+})
+export class CreateInteresovanjeComponent implements OnInit {
+
+    public newInteresovanje: Interesovanje = {
+        drzavljanstvo: '',
+        jmbg: 0,
+        punoIme: '',
+        email: '',
+        fiksni: 0,
+        mobilni: 0,
+        davalacKrvi: false,
+        opstinaVakcinacije: '',
+        opcije: []
+    };
+
+    public opcije: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.opcije = fb.group({
+            pfizer: false,
+            astra: false,
+            moderna: false,
+            sputnik: false,
+            sinopharm: false,
+            biloKoja: false
+
+        });
+    }
+
+    onSaveClick(): void {
+        if (!(this.newInteresovanje.drzavljanstvo && this.newInteresovanje.jmbg && this.newInteresovanje.punoIme &&
+            this.newInteresovanje.email && this.newInteresovanje.fiksni && this.newInteresovanje.mobilni && this.newInteresovanje.opstinaVakcinacije)) {
+            console.log("Please input all fields!");
+            return;
+        }
+
+        //TODO field validation
+        //todo checkbox validation
+
+
+
+    }
+
+    vaccineChosen(){
+        this.opcije.get("biloKoja")?.setValue(false);
+
+    }
+
+    anyChosen(){
+
+        if(this.opcije.get('biloKoja')?.value === true){
+            this.opcije = this.fb.group({
+                pfizer: false,
+                astra: false,
+                moderna: false,
+                sputnik: false,
+                sinopharm: false,
+                biloKoja: true
+            });
+        }
+        
+    }
+
+    ngOnInit(): void {
+    }
+
+}
