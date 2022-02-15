b4042d3ef27c32e21fe37182efe55713e556a2a2
StefanKrnajski <thegamingbook@live.com>
2022-02-15 21:20:26 +0100

    Change registration to include email, first name, last name, and password

--- portal/src/main/java/com/immunization/portal/dto/UserRegistrationDTO.java
+++ portal/src/main/java/com/immunization/portal/dto/UserRegistrationDTO.java
@@ -1,7 +1,10 @@
 package com.immunization.portal.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
@@ -15,11 +18,24 @@ import lombok.NoArgsConstructor;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "user_registration", propOrder = {
     "email",
-    "username",
+    "firstName",
+    "lastName",
     "password",
 })
 @XmlRootElement(name = "user_registration")
 public class UserRegistrationDTO {
+    @NotNull(message = "Email is missing")
+    @Email(message = "Email needs to be valid")
     private String email;
-    private String username, password;
+
+    @NotNull(message = "Password is missing")
+    private String password;
+    
+    @XmlElement(name = "first_name")
+    @NotNull(message = "First name is missing")
+    private String firstName;
+
+    @XmlElement(name = "last_name")
+    @NotNull(message = "Last name is missing")
+    private String lastName;
 }
--- portal/src/main/java/com/immunization/portal/service/UserService.java
+++ portal/src/main/java/com/immunization/portal/service/UserService.java
@@ -1,36 +1,66 @@
 package com.immunization.portal.service;
 
 import com.immunization.portal.exception.UserAlreadyExistsException;
+import com.immunization.portal.util.NumberStringGenerator;
+import com.immunization.common.dao.UserDAO;
+import com.immunization.common.model.User;
+import com.immunization.common.model.User.ERole;
+import com.immunization.common.util.TokenUtils;
 import com.immunization.portal.dto.UserRegistrationDTO;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserService {
 
-    public boolean registerUser(UserRegistrationDTO dto) throws UserAlreadyExistsException {
-        // Check if user exists
-        if (usernameTaken(dto.getUsername())) {
-            throw new UserAlreadyExistsException("Username already taken");
-        }
+    private final UserDAO userDAO;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenUtils tokenUtils;
+
+    private String generateUsername(UserRegistrationDTO dto) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(dto.getFirstName().replace(" ", "")).append('.').append(dto.getLastName().replace(" ", ""));
+        sb.append(NumberStringGenerator.generateNumberSequence(4));
+
+        return sb.toString();
+    }
+
+    public String registerUser(UserRegistrationDTO dto) throws UserAlreadyExistsException {
         if (emailTaken(dto.getEmail())) {
             throw new UserAlreadyExistsException("Email already taken");
         }
 
-        // TODO: Create user bean
-        // TODO: Save to DB
-        
-        return true;
+        // Generate unique password
+        String generatedUsername;
+        do {
+            generatedUsername = generateUsername(dto);
+        } while (usernameTaken(generatedUsername));
+
+        // Create user bean
+        User user = new User();
+        user.setEmail(dto.getEmail());
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setRole(ERole.GRADANIN);
+        user.setUsername(generatedUsername);
+
+        // Save to DB
+        userDAO.save(user);
+
+        return tokenUtils.generateToken(user.getUsername());
     }
 
     private boolean emailTaken(String email) {
-        // TODO: Check email exists
-        return false;
+        return userDAO.getByEmail(email).isEmpty() == false;
     }
 
     private boolean usernameTaken(String username) {
-        // TODO: Check username exists
-        return false;
+        return userDAO.getByUsername(username).isEmpty() == false;
     }
-    
+
 }
