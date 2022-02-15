a05632e86da17ef982b4e5f265377f7fd76e0c54
StefanKrnajski <thegamingbook@live.com>
2022-02-15 21:15:53 +0100

    Remove web mvc from application
    * Changed error controller with @ControllerAdvice

--- common/src/main/java/com/immunization/common/exception/base/BadRequestException.java
+++ common/src/main/java/com/immunization/common/exception/base/BadRequestException.java
@@ -1,4 +1,4 @@
-package com.immunization.common.exception;
+package com.immunization.common.exception.base;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- portal/src/main/java/com/immunization/portal/exception/UserAlreadyExistsException.java
+++ portal/src/main/java/com/immunization/portal/exception/UserAlreadyExistsException.java
@@ -1,10 +1,12 @@
 package com.immunization.portal.exception;
 
+import com.immunization.common.exception.base.ConflictException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class UserAlreadyExistsException extends RuntimeException {
+@ResponseStatus(HttpStatus.CONFLICT)
+public class UserAlreadyExistsException extends ConflictException {
     public UserAlreadyExistsException(String message) {
         super(message);
     }
