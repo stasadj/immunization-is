123339e2e1945b5f9aa2a1745dc38ae0168f767f
stasadj <anastasijadjuric@uns.ac.rs>
2022-02-16 21:09:48 +0100

    pred

--- common/src/main/java/com/immunization/common/model/digitalni_sertifikat/package-info.java
+++ common/src/main/java/com/immunization/common/model/digitalni_sertifikat/package-info.java
@@ -5,5 +5,6 @@
 // Generated on: 2022.02.16 at 11:58:33 AM CET 
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.uns.ac.rs/digitalni-sertifikat/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.uns.ac.rs/digitalni-sertifikat/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+		@javax.xml.bind.annotation.XmlNs(prefix = "pred", namespaceURI = "http://www.ftn.uns.ac.rs/rdf/predicate/") })
 package com.immunization.common.model.digitalni_sertifikat;
