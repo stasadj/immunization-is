aaecbaf8b9ba881bf07bd2d691ffce41b71fb24f
Dina Petrov <dina.petrov@gmail.com>
2022-02-16 11:58:17 +0100

    nillable Date for interesovanje and zahtev

--- common/src/main/java/com/immunization/common/model/util/Drzavljanstvo.java
+++ common/src/main/java/com/immunization/common/model/util/Drzavljanstvo.java
@@ -14,32 +14,34 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for status_zahteva_value.
+ * <p>Java class for drzavljanstvo.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * <p>
  * <pre>
- * &lt;simpleType name="status_zahteva_value"&gt;
+ * &lt;simpleType name="drzavljanstvo"&gt;
  *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
- *     &lt;enumeration value="NA CEKANJU"/&gt;
- *     &lt;enumeration value="ODOBREN"/&gt;
- *     &lt;enumeration value="PRIHVACEN"/&gt;
+ *     &lt;enumeration value="Državljanin Republike Srbije"/&gt;
+ *     &lt;enumeration value="Strani državljanin sa boravkom u RS"/&gt;
+ *     &lt;enumeration value="Strani državljanin bez boravka u RS"/&gt;
  *   &lt;/restriction&gt;
  * &lt;/simpleType&gt;
  * </pre>
  * 
  */
-@XmlType(name = "status_zahteva_value")
+@XmlType(name = "drzavljanstvo")
 @XmlEnum
-public enum StatusZahtevaValue {
+public enum Drzavljanstvo {
 
-    @XmlEnumValue("NA CEKANJU")
-    NA_CEKANJU("NA CEKANJU"),
-    ODOBREN("ODOBREN"),
-    PRIHVACEN("PRIHVACEN");
+    @XmlEnumValue("Dr\u017eavljanin Republike Srbije")
+    DRŽAVLJANIN_REPUBLIKE_SRBIJE("Dr\u017eavljanin Republike Srbije"),
+    @XmlEnumValue("Strani dr\u017eavljanin sa boravkom u RS")
+    STRANI_DRŽAVLJANIN_SA_BORAVKOM_U_RS("Strani dr\u017eavljanin sa boravkom u RS"),
+    @XmlEnumValue("Strani dr\u017eavljanin bez boravka u RS")
+    STRANI_DRŽAVLJANIN_BEZ_BORAVKA_U_RS("Strani dr\u017eavljanin bez boravka u RS");
     private final String value;
 
-    StatusZahtevaValue(String v) {
+    Drzavljanstvo(String v) {
         value = v;
     }
 
@@ -47,8 +49,8 @@ public enum StatusZahtevaValue {
         return value;
     }
 
-    public static StatusZahtevaValue fromValue(String v) {
-        for (StatusZahtevaValue c: StatusZahtevaValue.values()) {
+    public static Drzavljanstvo fromValue(String v) {
+        for (Drzavljanstvo c: Drzavljanstvo.values()) {
             if (c.value.equals(v)) {
                 return c;
             }
