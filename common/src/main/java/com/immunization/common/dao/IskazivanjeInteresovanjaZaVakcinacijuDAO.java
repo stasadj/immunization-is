0edecc78a8f20643b804b529bde9bfe189571e3b
stasadj <anastasijadjuric@uns.ac.rs>
2022-02-18 00:50:05 +0100

    double imports

--- common/src/main/java/com/immunization/common/dao/IskazivanjeInteresovanjaZaVakcinacijuDAO.java
+++ common/src/main/java/com/immunization/common/dao/IskazivanjeInteresovanjaZaVakcinacijuDAO.java
@@ -1,6 +1,7 @@
 package com.immunization.common.dao;
 
 import java.util.List;
+
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -12,44 +13,42 @@ import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 
-import java.util.Optional;
-
 @Component
 @AllArgsConstructor
 public class IskazivanjeInteresovanjaZaVakcinacijuDAO {
-	private final Exist exist;
-	private static final String INTEREST_NAMESPACE = "http://www.ftn.uns.ac.rs/interesovanje/";
+    private final Exist exist;
+    private static final String INTEREST_NAMESPACE = "http://www.ftn.uns.ac.rs/interesovanje/";
 
-	public long getNumberOfDocumentsOfInterest(String startDate, String endDate) throws Exception {
+    public long getNumberOfDocumentsOfInterest(String startDate, String endDate) throws Exception {
 
-		String xpathExp = "//inte:iskazivanje_interesovanja_za_vakcinaciju[number(translate(@datum,'-',''))>="
-				+ startDate + " and number(translate(@datum,'-',''))<=" + endDate + "]";
+        String xpathExp = "//inte:iskazivanje_interesovanja_za_vakcinaciju[number(translate(@datum,'-',''))>="
+                + startDate + " and number(translate(@datum,'-',''))<=" + endDate + "]";
 
-		return exist.count(xpathExp, IskazivanjeInteresovanjaZaVakcinaciju.class, INTEREST_NAMESPACE, "inte");
-	}
+        return exist.count(xpathExp, IskazivanjeInteresovanjaZaVakcinaciju.class, INTEREST_NAMESPACE, "inte");
+    }
 
     public Optional<IskazivanjeInteresovanjaZaVakcinaciju> retrieveById(String documentId) throws Exception {
-		IskazivanjeInteresovanjaZaVakcinaciju interesovanje = (IskazivanjeInteresovanjaZaVakcinaciju) exist
-				.retrieveById(documentId, IskazivanjeInteresovanjaZaVakcinaciju.class);
-		return interesovanje == null ? Optional.empty() : Optional.of(interesovanje);
-	}
+        IskazivanjeInteresovanjaZaVakcinaciju interesovanje = (IskazivanjeInteresovanjaZaVakcinaciju) exist
+                .retrieveById(documentId, IskazivanjeInteresovanjaZaVakcinaciju.class);
+        return interesovanje == null ? Optional.empty() : Optional.of(interesovanje);
+    }
 
     public List<IskazivanjeInteresovanjaZaVakcinaciju> getByUsername(String gradjaninUsername) throws Exception {
         String about = MetadataConstants.ABOUT_LICNI_PODACI_PREFIX + gradjaninUsername;
         String xpathExp = "//inte:iskazivanje_interesovanja_za_vakcinaciju[inte:pacijent[@about='" + about + "']]";
 
-        return  exist.query(xpathExp, IskazivanjeInteresovanjaZaVakcinaciju.class, INTEREST_NAMESPACE, "inte").stream()
-				.map(o -> (IskazivanjeInteresovanjaZaVakcinaciju) o).collect(Collectors.toList());
+        return exist.query(xpathExp, IskazivanjeInteresovanjaZaVakcinaciju.class, INTEREST_NAMESPACE, "inte").stream()
+                .map(o -> (IskazivanjeInteresovanjaZaVakcinaciju) o).collect(Collectors.toList());
 
-	}
+    }
 
-	public void save(String documentId, IskazivanjeInteresovanjaZaVakcinaciju interesovanje) throws Exception {
-		exist.save(documentId, interesovanje);
+    public void save(String documentId, IskazivanjeInteresovanjaZaVakcinaciju interesovanje) throws Exception {
+        exist.save(documentId, interesovanje);
 
-	}
+    }
 
-	public String getXML(String documentId) throws Exception {
-		return exist.retrieveRawXmlById(documentId, IskazivanjeInteresovanjaZaVakcinaciju.class);
-	}
+    public String getXML(String documentId) throws Exception {
+        return exist.retrieveRawXmlById(documentId, IskazivanjeInteresovanjaZaVakcinaciju.class);
+    }
 
 }
--- common/src/main/java/com/immunization/common/dao/ZahtevZaSertifikatDAO.java
+++ common/src/main/java/com/immunization/common/dao/ZahtevZaSertifikatDAO.java
@@ -1,6 +1,7 @@
 package com.immunization.common.dao;
 
 import java.util.List;
+
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -12,40 +13,37 @@ import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 
-import java.util.Optional;
-
 @Component
 @AllArgsConstructor
 public class ZahtevZaSertifikatDAO {
-	private final Exist exist;
-	private static final String REQUEST_NAMESPACE = "http://www.ftn.uns.ac.rs/zahtev-za-sertifikat/";
+    private final Exist exist;
+    private static final String REQUEST_NAMESPACE = "http://www.ftn.uns.ac.rs/zahtev-za-sertifikat/";
 
-	public long getNumberOfCertificateRequests(String startDate, String endDate) throws Exception {
+    public long getNumberOfCertificateRequests(String startDate, String endDate) throws Exception {
 
-		String xpathExp = "//zaht:datum_izdavanja[number(translate(text(),'-',''))>=" + startDate
-				+ " and number(translate(text(),'-',''))<=" + endDate + "]";
+        String xpathExp = "//zaht:datum_izdavanja[number(translate(text(),'-',''))>=" + startDate
+                + " and number(translate(text(),'-',''))<=" + endDate + "]";
 
-		return exist.count(xpathExp, ZahtevZaSertifikat.class, REQUEST_NAMESPACE, "zaht");
-	}
+        return exist.count(xpathExp, ZahtevZaSertifikat.class, REQUEST_NAMESPACE, "zaht");
+    }
 
-	public Optional<ZahtevZaSertifikat> retrieveById(String documentId) throws Exception {
-		ZahtevZaSertifikat zahtev = (ZahtevZaSertifikat) exist.retrieveById(documentId, ZahtevZaSertifikat.class);
-		return zahtev == null ? Optional.empty() : Optional.of(zahtev);
-	}
-
+    public Optional<ZahtevZaSertifikat> retrieveById(String documentId) throws Exception {
+        ZahtevZaSertifikat zahtev = (ZahtevZaSertifikat) exist.retrieveById(documentId, ZahtevZaSertifikat.class);
+        return zahtev == null ? Optional.empty() : Optional.of(zahtev);
+    }
 
     public List<ZahtevZaSertifikat> getByUsername(String gradjaninUsername) throws Exception {
         String about = MetadataConstants.ABOUT_LICNI_PODACI_PREFIX + gradjaninUsername;
         System.out.println(about);
         String xpathExp = "//zaht:zahtev_za_sertifikat[zaht:podnosilac_zahteva[@about='" + about + "']]";
 
-        return  exist.query(xpathExp, ZahtevZaSertifikat.class, REQUEST_NAMESPACE, "zaht").stream()
-				.map(o -> (ZahtevZaSertifikat) o).collect(Collectors.toList());
+        return exist.query(xpathExp, ZahtevZaSertifikat.class, REQUEST_NAMESPACE, "zaht").stream()
+                .map(o -> (ZahtevZaSertifikat) o).collect(Collectors.toList());
 
-	}
+    }
 
     public void save(String documentId, ZahtevZaSertifikat zahtev) throws Exception {
         exist.save(documentId, zahtev);
-		
-	}
+
+    }
 }
