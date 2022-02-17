78816ede378802dbcd09f07e0ef0f0faa50495d2
Dina Petrov <dina.petrov@gmail.com>
2022-02-17 11:31:40 +0100

    Added frontend form validations for Interesovanje and Zahtev creation

--- portal-frontend/src/app/components/create-interesovanje/create-interesovanje.component.ts
+++ portal-frontend/src/app/components/create-interesovanje/create-interesovanje.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Interesovanje } from '../../model/Interesovanje';
-import { Form, FormGroup, FormBuilder } from '@angular/forms';
+import { Form, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { InteresovanjeService } from 'src/app/services/interesovanje.service';
 import { ToastrService } from 'ngx-toastr';
 import { Drzavljanstvo } from 'src/app/model/Drzavljanstvo';
@@ -11,9 +11,11 @@ import { Drzavljanstvo } from 'src/app/model/Drzavljanstvo';
     styleUrls: ['./create-interesovanje.component.less'],
 })
 export class CreateInteresovanjeComponent implements OnInit {
+
+    //TODO delete default values after testing
     public newInteresovanje: Interesovanje = {
         drzavljanstvo: Drzavljanstvo['Državljanin Republike Srbije'],
-        jmbg: 1111111111119,
+        jmbg: 1111111111111,
         punoIme: 'Test Test',
         email: 'test@gmail.com',
         fiksni: 123111111,
@@ -24,10 +26,11 @@ export class CreateInteresovanjeComponent implements OnInit {
     };
 
     public opcije: FormGroup;
+    public newInteresovanjeFormGroup: FormGroup;
 
-    public srpsko = Drzavljanstvo['Državljanin Republike Srbije']
-    public saBoravkom = Drzavljanstvo['Strani državljanin sa boravkom u RS']
-    public bezBoravka = Drzavljanstvo['Strani državljanin bez boravka u RS']
+    public srpsko = Drzavljanstvo['Državljanin Republike Srbije'];
+    public saBoravkom = Drzavljanstvo['Strani državljanin sa boravkom u RS'];
+    public bezBoravka = Drzavljanstvo['Strani državljanin bez boravka u RS'];
 
     constructor(
         private fb: FormBuilder,
@@ -42,42 +45,85 @@ export class CreateInteresovanjeComponent implements OnInit {
             Sinopharm: false,
             'BILO KOJA': false,
         });
+
+        this.newInteresovanjeFormGroup = this.fb.group({
+            drzavljanstvo: [this.newInteresovanje.drzavljanstvo, Validators.required],
+            jmbg: [
+                this.newInteresovanje.jmbg,
+                [
+                    Validators.required,
+                    Validators.minLength(13),
+                    Validators.maxLength(13),
+                    Validators.pattern('[0-9]{13}'),
+                ],
+            ],
+            punoIme: [
+                this.newInteresovanje.punoIme,
+                Validators.required,
+            ],
+
+            email: [
+                this.newInteresovanje.email,
+                [Validators.required, Validators.email],
+            ],
+            mobilni: [
+                this.newInteresovanje.mobilni,
+                [Validators.required, Validators.pattern('[0-9]{9,11}')],
+            ],
+            fiksni: [
+                this.newInteresovanje.fiksni,
+                [Validators.required, Validators.pattern('[0-9]{9,10}')],
+            ],
+            opstinaVakcinacije: [
+                this.newInteresovanje.opstinaVakcinacije,
+                Validators.required,
+            ],
+            davalacKrvi: [
+                this.newInteresovanje.davalacKrvi,
+                Validators.required,
+            ],
+        });
+    }
+
+    ngAfterViewInit(){
+        
     }
 
     onSaveClick(): void {
-        if (!(
-                this.newInteresovanje.drzavljanstvo &&
-                this.newInteresovanje.jmbg &&
-                this.newInteresovanje.punoIme &&
-                this.newInteresovanje.email &&
-                this.newInteresovanje.fiksni &&
-                this.newInteresovanje.mobilni &&
-                this.newInteresovanje.opstinaVakcinacije
-            )
+
+        if (
+            !Object.keys(this.opcije.value).some((k) => !!this.opcije.value[k])
         ) {
-            this.toastr.error('Molimo Vas da popunite sva polja');
-            return;
-        }
-
-        //TODO individual fields validation
-
-        if (!Object.keys(this.opcije.value).some((k) => !!this.opcije.value[k])) {
             this.toastr.error('Morate odabrati barem jednu opciju vakcine');
             return;
         }
 
-        this.newInteresovanje.opcije = Object.keys(this.opcije.value).filter(
-            (key) => this.opcije.get(key)?.value === true
-        );
 
-        this.interesovanjeService
-            .create(this.newInteresovanje)
-            .subscribe((res) => {
-                this.toastr.success('Interesovanje uspešno zabeleženo. Proverite Vaš mejl.');
+        this.newInteresovanje = {
+            drzavljanstvo: this.newInteresovanjeFormGroup.value.drzavljanstvo,
+            jmbg: this.newInteresovanjeFormGroup.value.jmbg,
+            punoIme: this.newInteresovanjeFormGroup.value.punoIme,
+            email: this.newInteresovanjeFormGroup.value.email,
+            fiksni: this.newInteresovanjeFormGroup.value.fiksni,
+            mobilni: this.newInteresovanjeFormGroup.value.mobilni,
+            davalacKrvi: this.newInteresovanjeFormGroup.value.davalacKrvi,
+            opstinaVakcinacije: this.newInteresovanjeFormGroup.value.opstinaVakcinacije,
+            opcije:  Object.keys(this.opcije.value).filter(
+                (key) => this.opcije.get(key)?.value === true
+            ),
+        };
+
+
+        this.interesovanjeService.create(this.newInteresovanje).subscribe(
+            (res) => {
+                this.toastr.success(
+                    'Interesovanje uspešno zabeleženo. Proverite Vaš mejl.'
+                );
             },
             (error) => {
                 this.toastr.error('Već postoji interesovanje.'); //TODO add to intereceptor?
-            });
+            }
+        );
     }
 
     vaccineChosen() {
@@ -99,5 +145,5 @@ export class CreateInteresovanjeComponent implements OnInit {
         }
     }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {}
 }
--- portal-frontend/src/app/components/create-zahtev/create-zahtev/create-zahtev.component.ts
+++ portal-frontend/src/app/components/create-zahtev/create-zahtev/create-zahtev.component.ts
@@ -3,78 +3,121 @@ import { Pol } from 'src/app/model/Pol';
 import { ZahtevZaSertifikat } from 'src/app/model/ZahtevZaSertifikat';
 import { ToastrService } from 'ngx-toastr';
 import { ZahtevService } from 'src/app/services/zahtev.service';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import * as moment from 'moment';
 
-declare const Quill : any;
+declare const Quill: any;
 
 @Component({
     selector: 'app-create-zahtev',
     templateUrl: './create-zahtev.component.html',
     styleUrls: ['./create-zahtev.component.less'],
+    providers: [DatePipe],
 })
 export class CreateZahtevComponent implements OnInit {
+    public quill: any;
 
-
-    public quill : any;
+    //TODO: delete default values after testing
     public newZahtev: ZahtevZaSertifikat = {
         mestoIzdavanja: 'Novi Sad',
         imeIPrezime: 'Proba Proba',
         datumRodjenja: new Date('01-01-1999'),
-        pol: Pol.M,
+        pol: Pol["M"],
         jmbg: 1111111112345,
         brojPasosa: 111111111,
         razlog: 'Apsolventska ekskurzija',
     };
 
-    public muski = Pol.M;
-    public zenski = Pol.Z;
+    public newZahtevFormGroup: FormGroup;
+
+    public muski = Pol["M"];
+    public zenski = Pol["Ž"];
 
     constructor(
         private toastr: ToastrService,
-        private zahtevService: ZahtevService
-    ) {}
+        private zahtevService: ZahtevService,
+        private fb: FormBuilder,
+        private datePipe: DatePipe
+    ) {
+        this.newZahtevFormGroup = this.fb.group({
+            jmbg: [
+                this.newZahtev.jmbg,
+                [
+                    Validators.required,
+                    Validators.minLength(13),
+                    Validators.maxLength(13),
+                    Validators.pattern('[0-9]{13}'),
+                ],
+            ],
+            imeIPrezime: [this.newZahtev.imeIPrezime, Validators.required],
+
+            datumRodjenja: [
+                [
+                    this.datePipe.transform(
+                        this.newZahtev.datumRodjenja,
+                        'yyyy-MM-dd'
+                    ),
+                ],
+                [Validators.required, this.dateValidator()],
+            ],
+            pol: [this.newZahtev.pol, [Validators.required]],
+            brojPasosa: [
+                this.newZahtev.brojPasosa,
+                [Validators.required, Validators.pattern('[0-9]{9}')],
+            ],
+            mestoIzdavanja: [
+                this.newZahtev.mestoIzdavanja,
+                Validators.required,
+            ],
+        });
+    }
 
     ngOnInit(): void {}
 
     ngAfterViewInit(): void {
-    
         this.quill = new Quill('#editor', {
-            theme: 'snow'
-          });
-
-
-      }
+            theme: 'snow',
+        });
+    }
 
     onSaveClick() {
-        if (
-            !(
-                this.newZahtev.mestoIzdavanja &&
-                this.newZahtev.jmbg &&
-                this.newZahtev.imeIPrezime &&
-                this.newZahtev.pol &&
-                this.newZahtev.brojPasosa &&
-                this.newZahtev.razlog &&
-                this.newZahtev.datumRodjenja
-            )
-        ) {
-            this.toastr.error('Molimo Vas da popunite sva polja');
-            return;
-        }
+        this.newZahtev = {
+            mestoIzdavanja: this.newZahtevFormGroup.value.mestoIzdavanja,
+            imeIPrezime: this.newZahtevFormGroup.value.imeIPrezime,
+            datumRodjenja: this.newZahtevFormGroup.value.datumRodjenja,
+            pol: this.newZahtevFormGroup.value.pol,
+            jmbg: this.newZahtevFormGroup.value.jmbg,
+            brojPasosa: this.newZahtevFormGroup.value.brojPasosa,
+            razlog: this.quill.root.innerHTML, //getting razlog from quill
+        };
 
-        //TODO date of birth before today validation
-        //TODO other field validations
-
-        //get text from editor, set to object attr
-        let justHtml = this.quill.root.innerHTML;
-        console.log(justHtml);
-        this.newZahtev.razlog = justHtml;
-
-
-        this.zahtevService.create(this.newZahtev).subscribe(res => {
-            this.toastr.success("Zahtev za sertifikat uspešno poslat.");
-        })
+        this.zahtevService.create(this.newZahtev).subscribe((res) => {
+            this.toastr.success('Zahtev za sertifikat uspešno poslat.');
+        });
     }
 
     parseDate(dateString: string): Date {
         return new Date(dateString);
+    }
+
+    dateValidator(): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } | null => {
+            if (!(control && control.value)) {
+                // if there's no control or no value, that's ok
+                return null;
+            }
+
+            // return null if there's no errors
+            return moment().isBefore(moment(control.value))
+                ? { invalidDate: 'Datum rodjenja mora biti u proslosti' }
+                : null;
+        };
     }
 }
