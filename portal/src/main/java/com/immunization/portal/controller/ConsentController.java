af4a02836e69354d2344d8061668156bebe449fa
StefanKrnajski <thegamingbook@live.com>
2022-02-17 12:36:00 +0100

    Add authorization to immunization consent submission

--- portal/src/main/java/com/immunization/portal/controller/ConsentController.java
+++ portal/src/main/java/com/immunization/portal/controller/ConsentController.java
@@ -1,11 +1,14 @@
 package com.immunization.portal.controller;
 
+import com.immunization.common.model.User;
 import com.immunization.common.model.saglasnost.ObrazacSaglasnostiZaImunizaciju;
 import com.immunization.portal.service.ConsentService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +22,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ConsentController {
     private ConsentService consentService;
 
-    // TODO: Add authorization here for patients
     @PutMapping(value = "/")
-    public ResponseEntity<Void> immunizationConsent(@RequestBody ObrazacSaglasnostiZaImunizaciju form)
+    @PreAuthorize("hasRole('GRADANIN')")
+    public ResponseEntity<Void> immunizationConsent(@AuthenticationPrincipal User user, @RequestBody ObrazacSaglasnostiZaImunizaciju form)
             throws Exception {
         if (consentService.fileConsent(form)) {
             return new ResponseEntity<>(HttpStatus.OK);
