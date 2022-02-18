58ab20fa4ace4d305af421dc683c89fb867c8738
Dina Petrov <dina.petrov@gmail.com>
2022-02-18 14:28:09 +0100

    added generic document service to frontend portal

--- portal-frontend/src/app/components/gradjanin-documents/document-table/document-table.component.ts
+++ portal-frontend/src/app/components/gradjanin-documents/document-table/document-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { InteresovanjeService } from 'src/app/services/interesovanje.service';
 import { saveAs } from 'file-saver';
+import { DocumentService } from 'src/app/services/document-service';
 
 @Component({
     selector: 'app-document-table',
@@ -14,8 +15,11 @@ export class DocumentTableComponent implements OnInit {
         'open as XHTML',
         'open as PDF',
         'download XHTML',
+        'download PDF',
     ];
-    constructor(private interesovanjeService: InteresovanjeService) {}
+    constructor(
+        private documentService: DocumentService
+    ) {}
     ngOnInit(): void {}
 
     viewDoc(docName: string, format: string) {
@@ -27,19 +31,17 @@ export class DocumentTableComponent implements OnInit {
         );
     }
 
-    downloadDoc(docName: string) {
-        let [documentType, documentName] = docName.split('/');
-        if (documentType === 'interesovanje') {
-            this.interesovanjeService
-                .getPDF(documentName)
-                .subscribe((res: any) => {
-                    this.onReturnedDocumentForDownload(
-                        res,
-                        'application/pdf;charset=utf-8',
-                        'a.pdf'
-                    );
-                });
-        }
+    downloadDoc(fullDocName: string, documentFormat: string) {
+        let [documentType, documentName] = fullDocName.split('/');
+        this.documentService
+            .getFile(documentName, documentFormat, documentType)
+            .subscribe((res: any) => {
+                this.onReturnedDocumentForDownload(
+                    res,
+                    `application/${documentFormat};charset=utf-8`,
+                    `a.${documentFormat}`
+                );
+            });
     }
 
     onReturnedDocumentForDownload(
