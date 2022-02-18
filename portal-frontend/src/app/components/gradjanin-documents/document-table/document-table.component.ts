586c8b5a4f34648847ab5f466e322ea41a4e84d7
Dina Petrov <dina.petrov@gmail.com>
2022-02-18 14:07:20 +0100

    Started xhml download method

--- portal-frontend/src/app/components/gradjanin-documents/document-table/document-table.component.ts
+++ portal-frontend/src/app/components/gradjanin-documents/document-table/document-table.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { InteresovanjeService } from 'src/app/services/interesovanje.service';
 import { saveAs } from 'file-saver';
 
-
 @Component({
     selector: 'app-document-table',
     templateUrl: './document-table.component.html',
@@ -10,37 +9,45 @@ import { saveAs } from 'file-saver';
 })
 export class DocumentTableComponent implements OnInit {
     @Input() public documents: any[] = [];
-    public displayedColumns: string[] = ['name', 'open as XHTML', 'open as PDF', 'download XHTML'];
-    constructor(private interesovanjeService : InteresovanjeService) {}
+    public displayedColumns: string[] = [
+        'name',
+        'open as XHTML',
+        'open as PDF',
+        'download XHTML',
+    ];
+    constructor(private interesovanjeService: InteresovanjeService) {}
     ngOnInit(): void {}
 
-    viewDoc(docName : string, format: string){
-        let [documentType, documentName] = docName.split("/");
+    viewDoc(docName: string, format: string) {
+        let [documentType, documentName] = docName.split('/');
         console.log(documentType, documentName);
         window.open(
             `http://localhost:8080/api/${documentType}/${format}/${documentName}`,
             '_blank'
         );
-
     }
 
-    downloadDoc(docName : string){
-        // let [documentType, documentName] = docName.split("/");
-        // this.interesovanjeService
-        //     .getPDF('2312999850013.xml')
-        //     .subscribe((res: any) => {
-        //         this.onReturnedDocument(
-        //             res,
-        //             'application/pdf;charset=utf-8',
-        //             'a.pdf'
-        //         );
-        //     });
+    downloadDoc(docName: string) {
+        let [documentType, documentName] = docName.split('/');
+        if (documentType === 'interesovanje') {
+            this.interesovanjeService
+                .getPDF(documentName)
+                .subscribe((res: any) => {
+                    this.onReturnedDocumentForDownload(
+                        res,
+                        'application/pdf;charset=utf-8',
+                        'a.pdf'
+                    );
+                });
+        }
     }
 
-    onReturnedDocumentForDownload(res: any, type: string, filename: string): void {
+    onReturnedDocumentForDownload(
+        res: any,
+        type: string,
+        filename: string
+    ): void {
         let blob = new Blob([res], { type: type });
         saveAs(blob, filename);
     }
-
-
 }
