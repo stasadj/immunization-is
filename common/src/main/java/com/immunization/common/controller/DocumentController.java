16c696cc4d75488373863f00435654977f8d439d
albertmakan <makanalbert@gmail.com>
2022-02-18 10:32:17 +0100

    digitalni_sertifikat.xsl

--- common/src/main/java/com/immunization/common/controller/DocumentController.java
+++ common/src/main/java/com/immunization/common/controller/DocumentController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +21,6 @@ public abstract class DocumentController<T> {
     }
 
     @GetMapping(value = "/pdf/{documentId}", produces = MediaType.APPLICATION_PDF_VALUE)
-    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<InputStreamResource> getPdf(@AuthenticationPrincipal User user, @PathVariable String documentId) throws Exception {
         ByteArrayInputStream stream = documentService.generatePdf(documentId);
         if (stream == null)
@@ -37,7 +35,6 @@ public abstract class DocumentController<T> {
     }
 
     @GetMapping(value = "/xhtml/{documentId}", produces = MediaType.TEXT_HTML_VALUE)
-    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<InputStreamResource> getXhtml(@AuthenticationPrincipal User user, @PathVariable String documentId) throws Exception {
         ByteArrayInputStream stream = documentService.generateXhtml(documentId);
         if (stream == null)
