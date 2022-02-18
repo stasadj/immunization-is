34eb7934cbaca0115b3acfac59094f1512373b21
Dina Petrov <dina.petrov@gmail.com>
2022-02-18 14:03:41 +0100

    added buttons for viewing docs as xhtml or pdf, and download as xhtml

--- portal-frontend/src/app/components/gradjanin-documents/document-table/document-table.component.ts
+++ portal-frontend/src/app/components/gradjanin-documents/document-table/document-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { InteresovanjeService } from 'src/app/services/interesovanje.service';
+import { saveAs } from 'file-saver';
+
 
 @Component({
     selector: 'app-document-table',
@@ -8,23 +10,37 @@ import { InteresovanjeService } from 'src/app/services/interesovanje.service';
 })
 export class DocumentTableComponent implements OnInit {
     @Input() public documents: any[] = [];
-    public displayedColumns: string[] = ['name', 'view', 'download'];
+    public displayedColumns: string[] = ['name', 'open as XHTML', 'open as PDF', 'download XHTML'];
     constructor(private interesovanjeService : InteresovanjeService) {}
     ngOnInit(): void {}
 
-    viewDoc(docName : string){
+    viewDoc(docName : string, format: string){
         let [documentType, documentName] = docName.split("/");
         console.log(documentType, documentName);
         window.open(
-            `http://localhost:8080/api/${documentType}/xhtml/${documentName}`,
+            `http://localhost:8080/api/${documentType}/${format}/${documentName}`,
             '_blank'
         );
 
     }
 
     downloadDoc(docName : string){
-        console.log("downloading " + docName);
-        //TODO
+        // let [documentType, documentName] = docName.split("/");
+        // this.interesovanjeService
+        //     .getPDF('2312999850013.xml')
+        //     .subscribe((res: any) => {
+        //         this.onReturnedDocument(
+        //             res,
+        //             'application/pdf;charset=utf-8',
+        //             'a.pdf'
+        //         );
+        //     });
     }
+
+    onReturnedDocumentForDownload(res: any, type: string, filename: string): void {
+        let blob = new Blob([res], { type: type });
+        saveAs(blob, filename);
+    }
+
 
 }
